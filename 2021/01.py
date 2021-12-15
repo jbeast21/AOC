886bddad78418612929c0360d36b28eeea442578
JD <63008242+jbeast21@users.noreply.github.com>
2021-12-15 16:43:24 -0500

    Add files via upload

---
+++ 2021/01.py
@@ -0,0 +1,21 @@
+# Day 1
+
+fn = '01.txt'
+data = []
+for line in open(fn):
+    data.append(int(line.strip()))
+
+# Part 1
+inc = 0
+for ind, val in enumerate(data[:-1]):
+    if val < data[ind+1]:
+        inc += 1
+print(inc)
+
+# Part 2
+inc = 0
+for ind, val in enumerate(data[:-3]):
+    if sum(data[ind:ind+3]) < sum(data[ind+1:ind+4]):
+        inc += 1
+print(inc)
+
